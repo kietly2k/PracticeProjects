a954915501a4ef72074cf01cef7c56ebb4232d3b
Kiet Ly <kietly2k@gmail.com>
2023-09-04 23:37:15 +0700

    add comming soon page

---
+++ coming-soon-page/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'josefin-sans': ['Josefin Sans',  'sans-serif']
+      }
+    },
+    
+  },
+  plugins: [],
+}
+
